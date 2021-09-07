a997bdf814d4674a6617c2f7962cc1e7f5b1db0b
Joseph <josephtecson18@gmail.com>
2021-09-07 15:18:37 -0700

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import axios from'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const [pokemon, setPokemon] = useState([]); {
 
+const handleAPIcall = (event) => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+        .then(response => response.json(event))
+        .then(response => setPokemon(response.results))
+};
+
+return (
+    <div style={{textAlign: "center", marginTop: 10}}>
+        <button onClick= {handleAPIcall}>Fetch Pokemon</button>
+        {pokemon.length > 0 && pokemon.map((pokemon, index)=>{
+            return (<div key={index}>{pokemon.name}</div>)
+        })}
+    </div>
+);}
+
+}
 export default App;
